8cdf4f8eda8d84d1cfa02161040405efb4585c1d
parker lindley <fugazi8@gmail.com>
2018-03-25 17:25:22 -0600

    Functioning movobile version

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 document.querySelector('nav').onclick = (e) => {
-  const match = e.target.classList[1];
   if (!e.target.className.includes('active')) {
     document.querySelectorAll('.active').forEach(tab => tab.classList.remove('active'));
 
@@ -13,4 +12,21 @@ document.querySelector('nav').onclick = (e) => {
     })
     e.target.classList.add('active');
   } 
+}
+
+document.querySelector('.mobile').onclick = (e) => {
+  if (!e.target.className.includes('active')) {
+    document.querySelectorAll('.active').forEach(tab => tab.classList.remove('active'));
+
+    document.querySelectorAll('.article').forEach(article => {
+      article.classList.remove('displayed');
+      if (article.id.includes(e.target.classList[1])) {
+        article.classList.add('displayed');
+        document.querySelectorAll('.expand').forEach(symbol => symbol.innerText = '-')
+        e.target.children[0].innerText = "+";
+      }
+    })
+    e.target.classList.add('active');
+  } 
+  
 }
