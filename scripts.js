f5229ca7c8c0bdd13c532c313c6b6f229090c65d
parker lindley <fugazi8@gmail.com>
2018-03-25 17:36:06 -0600

    Accordian tabs complete

--- scripts.js
+++ scripts.js
@@ -1,32 +1,23 @@
 document.querySelector('nav').onclick = (e) => {
+  selectTabs(e);
+}
+
+document.querySelector('.mobile').onclick = (e) => {
+  selectTabs(e);
+}
+
+const selectTabs = (e) => {
   if (!e.target.className.includes('active')) {
-    document.querySelectorAll('.active').forEach(tab => tab.classList.remove('active'));
+    document.querySelectorAll('.active').forEach( tab => tab.classList.remove('active'));
 
     document.querySelectorAll('.article').forEach( article => {
       article.classList.remove('displayed');
       if (article.id.includes(e.target.classList[1])) {
         article.classList.add('displayed');
-        document.querySelectorAll('.expand').forEach( symbol => symbol.innerText = '-')
+        document.querySelectorAll('.expand').forEach(symbol => symbol.innerText = '-');
         e.target.children[0].innerText = "+";
       }
-    })
+    });
     e.target.classList.add('active');
   } 
-}
-
-document.querySelector('.mobile').onclick = (e) => {
-  if (!e.target.className.includes('active')) {
-    document.querySelectorAll('.active').forEach(tab => tab.classList.remove('active'));
-
-    document.querySelectorAll('.article').forEach(article => {
-      article.classList.remove('displayed');
-      if (article.id.includes(e.target.classList[1])) {
-        article.classList.add('displayed');
-        document.querySelectorAll('.expand').forEach(symbol => symbol.innerText = '-')
-        e.target.children[0].innerText = "+";
-      }
-    })
-    e.target.classList.add('active');
-  } 
-  
 }
