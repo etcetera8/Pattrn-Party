f4e55221e5c7ba491c5d7116d0108d29e89351b3
parker lindley <fugazi8@gmail.com>
2018-03-25 15:15:04 -0600

    Add functional expansion notifications to tabs

--- scripts.js
+++ scripts.js
@@ -1,17 +1,16 @@
 document.querySelector('nav').onclick = (e) => {
   const match = e.target.classList[1];
   if (!e.target.className.includes('active')) {
-    document.querySelectorAll('.active').forEach(tab => {
-      tab.classList.remove('active')
-    })
+    document.querySelectorAll('.active').forEach(tab => tab.classList.remove('active'));
 
     document.querySelectorAll('.article').forEach( article => {
-      article.classList.remove('displayed')
+      article.classList.remove('displayed');
       if (article.id.includes(e.target.classList[1])) {
-        article.classList.add('displayed')
+        article.classList.add('displayed');
+        document.querySelectorAll('.expand').forEach( symbol => symbol.innerText = '-')
+        e.target.children[0].innerText = "+";
       }
-      
     })
-    e.target.classList.add('active')
+    e.target.classList.add('active');
   } 
 }
